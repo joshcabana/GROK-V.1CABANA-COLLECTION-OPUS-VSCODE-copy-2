654f1c146f87b71a56057ae5d12741b18ffd659b
joshcabana <cabana.collections2025@gmail.com>
2026-02-05 07:48:45 +1100

    Refactor Lighthouse CI configuration: update URL definitions and restore server command

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -2,11 +2,12 @@ module.exports = {
   ci: {
     collect: {
       numberOfRuns: 3,
+      startServerCommand: "npx http-server dist -p 4173 -c-1",
+      startServerReadyPattern: "Available on",
       url: [
-        "index.html",
-        "products/mens-boxer-brief-black.html"
+        "http://127.0.0.1:4173/",
+        "http://127.0.0.1:4173/products/mens-boxer-brief-black.html"
       ],
-      staticDistDir: "dist",
       settings: { preset: "perf", formFactor: "mobile", chromeFlags: "--no-sandbox" }
     },
     assert: {
