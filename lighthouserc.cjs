12194b311aa3e8e2c07a3d59360073fad5235e4d
joshcabana <cabana.collections2025@gmail.com>
2026-02-05 07:39:42 +1100

    Refactor Lighthouse CI configuration to remove server commands and streamline URL definitions

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -2,12 +2,11 @@ module.exports = {
   ci: {
     collect: {
       numberOfRuns: 3,
-      startServerCommand: "npx http-server dist -p 4173 -c-1",
-      startServerReadyPattern: "Available on",
       url: [
-        "http://127.0.0.1:4173/",
-        "http://127.0.0.1:4173/products/mens-boxer-brief-black.html"
+        "index.html",
+        "products/mens-boxer-brief-black.html"
       ],
+      staticDistDir: "dist",
       settings: { preset: "perf", formFactor: "mobile", chromeFlags: "--no-sandbox" }
     },
     assert: {
